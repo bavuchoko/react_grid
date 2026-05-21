4f6f6af75244ded593503b7f45b8bc96c195d687
pjs <bavuchoko@naver.com>
2026-05-21 14:07:51 +0900

    [update] 커스텀 아이콘을 추가할 수 있게 되서 기존의 onCreate, onUpload, onDownload 제거

--- src/app/index.ts
+++ src/app/index.ts
@@ -14,7 +14,6 @@ export { resolveJsGridTheme, gridThemeStyles } from "./js-grid/gridTheme.ts";
 
 export { default as JsGrid } from "./JsGrid.tsx";
 export { default as ToolbarAsyncAction } from "./js-grid/ToolbarAsyncAction.tsx";
-export { default as ToolbarUploadAction } from "./js-grid/ToolbarUploadAction.tsx";
 export { useJsGridToolbar } from "./js-grid/JsGridToolbarContext.tsx";
 export { DEFAULT_EXCEL_UPLOAD_ACCEPT } from "./js-grid/excelUploadConstraints.ts";
 
